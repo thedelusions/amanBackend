5f0054c85655255b4873e9ef6681c1c3088ca782
RawanAli <rawan.ali.fardan@email.com>
2025-11-09 01:59:06 +0300

    delete route user.js

--- controllers/users.js
+++ controllers/users.js
@@ -6,19 +6,23 @@ const User = require('../models/user');
 //get user profile
 router.get('/:userId', async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId).select('-hashedPassword');
+    const user = await User.findById(req.params.userId);
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
-    res.status(200).json(user);
+    res.status(200).json(user._id);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
 //update user profile
-router.put('/:id', verifyToken, async (req, res) => {
+router.put('/:id/edit', verifyToken, async (req, res) => {
   try {
+
+     if (req.user._id !== req.params.id) {
+      return res.status(404).json({ error: 'Access denied' });
+    }
 
     const { name, phone, area, email } = req.body;
     const updatedData = { name, phone, area };
@@ -35,7 +39,7 @@ router.put('/:id', verifyToken, async (req, res) => {
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
       updatedData,
-    ).select('-hashedPassword');
+    );
 
     res.status(200).json(updatedUser);
   } catch (error) {
@@ -43,5 +47,21 @@ router.put('/:id', verifyToken, async (req, res) => {
   }
 });
 
+// delete user profile
+router.delete('/:id', async (req, res) => {
+  try {
+
+     if (req.user._id !== req.params.id) {
+      return res.status(404).json({ error: 'Access denied' });
+    }
+
+    const deletedUser = await User.findByIdAndDelete(req.user._id);
+    if (!deletedUser) return res.status(404).json({ error: 'User not found' });
+
+    res.status(200).json({ message: 'Account deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
